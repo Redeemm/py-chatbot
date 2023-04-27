ddf44ac009542f6927b9b346d161954087415a12
Redeemm <71359434+Redeemm@users.noreply.github.com>
2023-04-27 09:42:54 +0000

    Add files via upload

---
+++ ch.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify, render_template
+import random
+import json
+import torch
+
+
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+app = Flask(__name__)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Sam"
+
+@app.get("/")
+def index_get():
+    return render_template("base.html")
+
+@app.route('/chat', methods=['POST'])
+def chat():
+
+    message = request.json['message']
+
+    sentence = tokenize(message)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                response = random.choice(intent['responses'])
+                message = {'answer': response}
+                return jsonify(message)
+    else:
+        error = {'answer': 'I do not understand...'}
+        return jsonify(error)
+
+    # mesaage = {"answer": response}
+    # return jsonify(mesaage)
+if __name__ == '__main__':
+    app.run(debug=True)
